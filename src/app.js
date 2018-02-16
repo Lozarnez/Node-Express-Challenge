6742f2a2c96934310c4badd4334ff7aea5b6b21a
Julian Kohlman <juliankohlman@gmail.com>
2018-02-16 14:07:18 -0500

    Fix unhandled promise rejection warning. Minor refactoring

--- src/app.js
+++ src/app.js
@@ -11,12 +11,7 @@ const STATUS_USER_ERROR = 422;
 
 const currentBTC = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json';
 const prevBTC = 'https://api.coindesk.com/v1/bpi/historical/close.json?currency=USD&for=yesterday';
-// endpoint '/compare'
-  // return amount in USD that bitcoin has risen or fallen
-// two requests
-  // previous days btc price
-  // current days btc price
-// const btc = [];
+
 function get(url) {
   return new Promise((resolve, reject) => {
     fetch(url)
@@ -31,53 +26,17 @@ app.get('/current', (req, res) => {
     get(currentBTC),
     get(prevBTC)
   ]).then(([current, prev]) => {
-    // res.send({
-    //   current: current.bpi.USD.rate,
-    //   prev: prev.bpi
-    // })
-    let c = current.bpi.USD.rate_float;
-    // .bpi.USD.rate;
-    let p = Object.values(prev.bpi)[0];
-    let diff = c - p
-    res.send({c, p, diff})
-    .catch(err => res.status(STATUS_USER_ERROR).send( {err: err }));
+    const currentDayPrice = current.bpi.USD.rate_float;
+    const previousDayPrice = Object.values(prev.bpi)[0];
+    const priceDifference = currentDayPrice - previousDayPrice;
+    res.send({
+      priceDifference,
+      currentDayPrice,
+      previousDayPrice
+
+    })
   })
 })
-// app.get('/current', (req, res) => {
-//   fetch(currentBTC)
-//     .then(current => current.json())
-//     .then(current => {
-//       // console.log(current);
-//       // console.log(btc);
-//       res.status(STATUS_SUCCESS).send(current.bpi.USD.rate);
-//       btc.push(current.bpi.USD.rate);
-//       console.log(btc);
-//     })
-//     .catch(err => {
-//       res.status(STATUS_USER_ERROR).send( {err: err });
-//     });
-// })
-
-// const lastBTC = [];
-
-// app.get('/previous', (req, res) => {
-//   fetch(prevBTC)
-//     .then(previous => previous.json())
-//     .then(previous => {
-//       console.log(previous);
-//       console.log(Object.values(previous.bpi));
-//       res.status(STATUS_SUCCESS).send(previous.bpi);
-//       // btc.push(previous);
-//       // console.log(btc);
-//     })
-//     .catch(err => {
-//       res.status(STATUS_USER_ERROR).send( {err: err });
-//     });
-// })
-
-
-
-
 
 app.listen(PORT, err => {
   if (err) {
