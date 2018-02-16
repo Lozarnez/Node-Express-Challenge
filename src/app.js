9b735aae7f0ee97088370bbd01463fdb33f5ae18
Julian Kohlman <juliankohlman@gmail.com>
2018-02-16 12:45:02 -0500

    Able to retrieve current and previous days btc price. Refactor response code and return btc price diff

--- src/app.js
+++ src/app.js
@@ -9,6 +9,51 @@ const PORT = config.port
 const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
 
+const currentBTC = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json';
+const prevBTC = 'https://api.coindesk.com/v1/bpi/historical/close.json?currency=USD&for=yesterday';
+// endpoint '/compare'
+  // return amount in USD that bitcoin has risen or fallen
+// two requests
+  // previous days btc price
+  // current days btc price
+const btc = [];
+
+app.get('/current', (req, res) => {
+  fetch(currentBTC)
+    .then(current => current.json())
+    .then(current => {
+      // console.log(current);
+      // console.log(btc);
+      res.status(STATUS_SUCCESS).send(current.bpi.USD.rate);
+      btc.push(current.bpi.USD.rate);
+      console.log(btc);
+    })
+    .catch(err => {
+      res.status(STATUS_USER_ERROR).send( {err: err });
+    });
+})
+
+const lastBTC = [];
+
+app.get('/previous', (req, res) => {
+  fetch(prevBTC)
+    .then(previous => previous.json())
+    .then(previous => {
+      console.log(previous);
+      console.log(Object.values(previous.bpi));
+      res.status(STATUS_SUCCESS).send(previous.bpi);
+      // btc.push(previous);
+      // console.log(btc);
+    })
+    .catch(err => {
+      res.status(STATUS_USER_ERROR).send( {err: err });
+    });
+})
+
+
+
+
+
 app.listen(PORT, err => {
   if (err) {
     console.log(`error starting server: ${err}`)
